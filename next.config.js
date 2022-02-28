56f2a2b16f9cd1f4896d0137ed4f67de81692801
Kai <kaicoleridge13@gmail.com>
2022-02-28 19:56:55 +0000

    main

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  redirects() {
-    return [
-      process.env.MAINTENANCE_MODE === "1"
-        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
-        : null,
-    ].filter(Boolean);
-  }
-};
+module.exports = nextConfig
