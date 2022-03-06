2bc4a6b3287a9068d118366916ef5c6d70a73f14
Kai <kaicoleridge13@gmail.com>
2022-03-06 17:08:27 +0000

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="##7050e2"></meta>
       </Head>
       <Layout>
         <Component {...pageProps} />
